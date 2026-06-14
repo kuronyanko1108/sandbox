9dfe98e8511ace88885ce3332fc1d72df67311c3
kuronyanko <sakanoueno1108@gmail.com>
2026-06-14 16:30:15 +0900

    AnkiConnect のノート追加処理を試作

---
+++ study/anki_connect/api_study.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+
+def request(action, **params):
+    return requests.post(
+        "http://localhost:8765", json={"action": action, "params": params, "version": 6}
+    ).json()
+
+
+def search_cards(query):
+    return request("findCards", query=query)
+
+
+def add_note(deck_name, front, back, tags=None):
+    if tags is None:
+        tags = []
+
+    request("createDeck", deck=deck_name)
+
+    return request(
+        "addNote",
+        note={
+            "deckName": deck_name,
+            "modelName": "基本",
+            "fields": {"表面": front, "裏面": back},
+            "tags": tags,
+        },
+    )
+
+
+response = request("modelFieldNames", modelName="基本")
+print(response)
+
+cards = search_cards("tag:コンピュータ構成要素")
+print(cards)
+
+
+result = add_note("日本語", "りんご", "apple", ["食べ物", "果物"])
+print(result)
