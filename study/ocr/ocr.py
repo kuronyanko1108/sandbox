eeb5afd24adb5cc51e69b05b448a8003bcdd1b7e
kuronyanko <sakanoueno1108@gmail.com>
2026-06-24 12:05:32 +0900

    形態要素解析処理とJSON読み込み処理の実験

--- study/ocr/ocr.py
+++ study/ocr/ocr.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import pyocr
 import pyocr.builders
+from janome.tokenizer import Tokenizer
+import json
 
 # Tesseractの実行ファイルへのパスを指定
 pyocr.tesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
@@ -11,7 +13,7 @@ tool = tools[0]
 
 # 画像のファイルを指定する
 img_path = (
-    "C:/Users/Sakan/dev/anki-snap-importer/capture/completed/20260623_question_008.png"
+    "C:/Users/Sakan/dev/anki-snap-importer/capture/completed/20260623_question_004.png"
 )
 img = Image.open(img_path)
 
@@ -19,9 +21,21 @@ img = Image.open(img_path)
 builder = pyocr.builders.TextBuilder(tesseract_layout=6)
 txt = tool.image_to_string(img, lang="eng+jpn", builder=builder).replace(" ", "")
 
-builder = pyocr.builders.WordBoxBuilder(tesseract_layout=6)
-boxs = tool.image_to_string(img, lang="eng+jpn", builder=builder)
 
+# 形態素解析
+t = Tokenizer("user.csv", udic_type="simpledic", udic_enc="utf8")
+words_set = set(token for token in t.tokenize(txt, wakati=True))
 
-print(txt)
-print("Yes" if ">データベース" in txt else "No")
+# JSONファイルの読み込み
+with open("config.json", mode="r", encoding="utf-8") as f:
+    tag_mapping = json.load(f)
+
+# タグの設定
+tag_set = set()
+for word in words_set:
+    if word in tag_mapping:
+        tag_set.update(tag_mapping[word])
+
+anki_tags = list(tag_set)
+
+print(anki_tags)
